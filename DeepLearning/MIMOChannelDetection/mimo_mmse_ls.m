84aaaba9d695d677fa8a19199cdca13edbae9f28
RushabhaB <f20170220@pilani.bits-pilani.ac.in>
2020-12-02 06:42:54 +0000

    Changes made to incorporate changing SNR

--- DeepLearning/MIMOChannelDetection/mimo_mmse_ls.m
+++ DeepLearning/MIMOChannelDetection/mimo_mmse_ls.m
@@ -1,9 +1,9 @@
-function [H_est_ls, H_est_mmse] = mimo_mmse_ls(Ncarr,Nant,Rx,pilots,pilot_channel,pilot_columns)
+function [Hls, Hmmse] = mimo_mmse_ls(Ncarr,Nant,Rx,pilots,pilot_channel,pilot_columns)
 
 % LS estimation
 Hls= Rx./pilots;
 % MMSE Channel Estimation
-R_HH = pilot_channel*pilot_channel'; 
+R_HH = pilot_channel'*pilot_channel; 
 XX = toeplitz(pilots);     
 powerDB = 10*log10(var(Rx)); % Calculate Tx signal power
 sigmI = 10.^(0.1*(powerDB)); % Calculate the noise variance
@@ -11,7 +11,7 @@ if(norm(R_HH,'fro')==0)
     Hmmse = zeros(size(Hls));
 else
     G=(R_HH)*(R_HH + (1./sigmI).*(XX))^(-1);
-    Hmmse=reshape(((G)*(Hls)),size(Hls));
+    Hmmse=reshape(((Hls)*(G)),size(Hls));
 end
     
 
@@ -22,6 +22,6 @@ y = 1:Nant;
 x_2d = 1:Ncarr;
 y_2d = 1:Nant;
 [X_2d,Y_2d] = meshgrid(x_2d,y_2d);
-H_est_ls = interp2(X,Y,Hls,X_2d,Y_2d,'spline');
-H_est_mmse = interp2(X,Y,Hmmse,X_2d,Y_2d,'spline');
+%H_est_ls = interp2(X,Y,Hls,X_2d,Y_2d,'spline');
+%H_est_mmse = interp2(X,Y,Hmmse,X_2d,Y_2d,'spline');
 end
